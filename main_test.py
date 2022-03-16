4d602d5e8a2705387958c4c01d4a9d69f4ced187
faott <faott@github.com>
2022-03-16 21:47:04 +0100

    Change player behavior

--- main_test.py
+++ main_test.py
@@ -1,4 +1,3 @@
-from curses import KEY_LEFT, KEY_RIGHT
 import pygame
 import random
 
@@ -37,14 +36,19 @@ enemy_size = 30
 enemy_speed = 15
 enemy_pos_y = 0 + enemy_size
 enemy_pos_x = 0 + enemy_size
+last_enemy_pos_x = 700
+
+enemy_count = 3
+enemy_offset = (last_enemy_pos_x - enemy_pos_x ) // (enemy_count -1)
+
 enemy_colour = RED
 
 player_width = 100
 player_height = 20
 player_pos_x = WIDTH//2 - player_width//2
 player_pos_y = HEIGHT - player_height
-player_speed = 8
 
+offset = 0
 
 
 while running:
@@ -61,41 +65,29 @@ while running:
     # Draw game:
     screen.fill(WHITE)
 
-
-    enemy1 = pygame.draw.circle(screen, enemy_colour, (int(enemy_pos_x), enemy_pos_y), enemy_size)
-    enemy2 = pygame.draw.circle(screen, enemy_colour, (int(enemy_pos_x + 200), enemy_pos_y), enemy_size)
-    enemy3 = pygame.draw.circle(screen, enemy_colour, (int(enemy_pos_x + 400), enemy_pos_y), enemy_size)
+    for e in range(enemy_count):
+        pygame.draw.circle(screen, enemy_colour, (int(enemy_pos_x + offset), enemy_pos_y), enemy_size)
+        offset += enemy_offset
+    
     enemy_pos_x += enemy_speed
 
-    if enemy_pos_x + 400 >= WIDTH - enemy_size or enemy_pos_x <= 0 + enemy_size:
+    if enemy_pos_x + last_enemy_pos_x >= WIDTH - enemy_size or enemy_pos_x <= 0 + enemy_size:
         enemy_speed *= -1*1.1
         enemy_pos_y += random.randint(10,20)
         enemy_colour = random.choice(colours_list[1:])
-    
+
+
+
+
+
+
+
+
+
+
     # Draw Player
+
     pygame.draw.rect(screen, BLUE, (player_pos_x, player_pos_y, player_width, player_height))
-    keys = pygame.key.get_pressed()
-    move_x = 0
-    move_y = 0
-    
-    if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
-        move_x = player_speed
-    if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
-        move_x -= player_speed
-    if keys[pygame.K_UP] and not keys[pygame.K_DOWN] and player_pos_y > 0:
-        move_y -= player_speed
-    if keys[pygame.K_DOWN] and not keys[pygame.K_UP] and player_pos_y + player_height < HEIGHT:
-        move_y = player_speed
-
-    player_pos_x += move_x
-    player_pos_y += move_y
-
-    if player_pos_x < 0:
-        player_pos_x = WIDTH - player_width
-    elif player_pos_x > WIDTH - player_width:
-        player_pos_x = 0
-
-    
 
     # Finally: Update the display
     pygame.display.update()
