1f1957e688d8e0861237bf643e015de8816f50f0
faott <faott@github.com>
2022-03-21 22:53:48 +0100

    Various small changes

--- main.py
+++ main.py
@@ -16,15 +16,13 @@ from rockets import Rockets
 
 pygame.init()  # type: ignore
 
-pygame.display.set_caption('PLAYGROUND')
+pygame.display.set_caption('INVADERS')
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
 background = pygame.image.load("media/background.png").convert_alpha()
-
-
-
+game_over_surf = pygame.image.load("media/game_over.png").convert_alpha()
 
 
 # ---------
@@ -39,10 +37,12 @@ player1 = Player(50, 50)
 shots = []
 enemys = []
 enemy_offset = 0
+score = 0
 
-for x in range(3):
-    enemy = Enemy(0 + enemy_offset, 30, 5, 30)
-    enemys.append(enemy)
+
+
+for x in range(random.randint(5,5)):
+    enemys.append(Enemy(0 + enemy_offset, 30, 5, 30))
     enemy_offset += 100
 
 
@@ -59,6 +59,8 @@ def collision(cx1, cx2, cy1, cy2, r1, r2):
 
 enemy_shooting = pygame.USEREVENT + 1
 pygame.time.set_timer(enemy_shooting, random.randint(900, 1500))
+
+
 
 
 while running:
@@ -87,14 +89,14 @@ while running:
             enemy_shot = Rockets(shooter.x + shooter.size/2, shooter.y + shooter.size + 5, 15, 5)
             shots.append(enemy_shot)
     
-   # Enemy shots alternative
+    # Enemy shots alternative
 
     for e in enemys:
         if random.randint(1,50) == 2:
             enemy_shot = Rockets(e.x + e.size/2, e.y + e.size + 5, 15, 5)
             shots.append(enemy_shot)
 
-   # Player movement
+   # Player movement reset to zero
 
     player1.vx = 0
     player1.vy = 0
@@ -112,18 +114,20 @@ while running:
     player1.update()
     player1.draw(screen)
 
-    # Checking the shots and the enemys for collision
+    # Checking the shots if they collide with the player
 
     for s in shots:
-        if collision((player1.x + player1.width / 2), s.x, (player1.y + player1.height / 2), s.y, player1.width, s.size):
+        if collision((player1.x + player1.width / 2), s.x, (player1.y + player1.height / 2), s.y, player1.width / 2, s.size):
             pygame.quit()
             exit()
 
+    # Checking if the shots collide with enemys but only if the shots are go upwards (no enemy friendly fire)
         else:
             for e in enemys:
-                if collision(e.x, s.x, e.y, s.y, e.size, s.size):
+                if collision(e.x, s.x, e.y, s.y, e.size, s.size) and s.speed < 0:
                     s.destroyed = True
                     e.destroyed = True
+                    score += 1
                 else:
                     pass
 
